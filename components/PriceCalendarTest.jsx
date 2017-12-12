6ff5bc3f9a1a21f299796f243262348a8f399e8b
samir <samir@b;uewall.jpn.com>
2017-12-12 19:06:43 +0900

    Commit for added React JS files for sample.

--- components/PriceCalendarTest.jsx
+++
@@ -1,5 +0,0 @@
-import Price  from 'PriceCalendar';
-
-test('adds 1 + 2 to equal 3', () => {
-    expect(Price.getUrlId("www.hotmail.com/test")).toBe(3);
-});
--- components/modules/country.js
+++
@@ -1,7 +0,0 @@
-exports.JP = [
-	{ value: 'JP', label: 'Japan' },
-	{ value: 'AU', label: 'Australia' },
-    { value: 'US', label: 'USA' },
-
-];
-
