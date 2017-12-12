987b8c957075279beeabacd522108f9fe6930a24
samirbije <samirbije@gmail.com>
2017-12-12 18:34:08 +0900

    Delete testold.js

--- testold.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {observable} from 'mobx';
-import Test from "./components/Test";
-import Test1 from "./components/Test1";
-import appState from "./mobx/mobx1";
-
-ReactDOM.render(<Test1 appState={appState} />,document.getElementById("test1"));
-ReactDOM.render(<Test appState={appState} />,document.getElementById("test"));
